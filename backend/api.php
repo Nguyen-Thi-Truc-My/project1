06e68bed9eeb23a6f5289969e5929f5634ce710c
NguyenThiTrucMy <trucmy10042004@gmail.com>
2025-12-15 14:23:31 +0700

    Thong diep 8

--- backend/api.php
+++ backend/api.php
@@ -2,18 +2,17 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-header("Access-Control-Allow-Origin: *"); // cho phép domain khác gọi
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-// Thông tin DB InfinityFree
-$host = "sql205.infinityfree.com";
-$user = "if0_40684966";
-$pass = "lF9REx2NKJi";
-$dbname = "if0_40684966_project1";
+// Thông tin DB (Render hoặc external MySQL)
+$host = "your-db-host";    
+$user = "your-db-user";
+$pass = "your-db-pass";
+$dbname = "your-db-name";
 
 // Kết nối DB
 $conn = new mysqli($host, $user, $pass, $dbname);
-
 if ($conn->connect_error) {
     echo json_encode([
         "status" => "error",
